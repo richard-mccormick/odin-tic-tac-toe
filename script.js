8c73cbb8e670cf080b450f19f5e17a039b5973c7
Richard McCormick <mccormickri38@gmail.com>
2024-06-05 20:02:44 -0400

    Add gameController factory. Add script to make game playable in console

--- script.js
+++ script.js
@@ -1,1 +1,65 @@
 console.log("IT'S WORKING!!");
+
+const gameBoard = (function() {
+    let board = [
+        "","","",
+        "","","",
+        "","",""];
+    const getBoard = () => board;
+
+    const clearBoard = () => {board = ["","","","","","","","",""]};
+
+
+    return {getBoard, clearBoard};
+})();
+
+function createPlayer(name, symbol) {
+    return {name, symbol};
+}
+
+const gameController = (function(){
+    
+
+    const players = [createPlayer("Player 1", "O"), createPlayer("Player 2", "X")];
+    let playBoard = gameBoard.getBoard();
+
+    const winningPositions = [
+        //horizontal
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        //vertical
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        //diagonal
+        [0,4,8],
+        [2,4,6]
+        
+    ];
+
+    const checkWin = () => {
+        for(const position of winningPositions){
+            if(playBoard[position[0]] != "" && 
+            (playBoard[position[0]] === playBoard[position[1]] && 
+            playBoard[position[0]] === playBoard[position[2]])){
+                return true;
+            }
+
+        }
+    }
+
+
+    const mark = (symbol, space) => {
+        playBoard[space] = symbol;
+        if(checkWin() === true){
+            console.log("YOU WON!!!!");
+            gameBoard.clearBoard();
+            playBoard = gameBoard.getBoard();
+            console.log(playBoard);
+        }
+    }
+    
+
+    return {players, mark, /*checkWin*/};
+})();
